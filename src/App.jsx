9ecc7edf616c6b2c27309a4214ebff086d3d6cd1
amosmurmu <amosmurmu002@gmail.com>
2024-11-14 13:48:53 +0530

    completed react basic todo app

--- src/App.jsx
+++ src/App.jsx
@@ -4,9 +4,26 @@ import FilterButton from "./components/FilterButton";
 import { useState } from "react";
 import { nanoid } from "nanoid";
 
+const FILTER_MAP = {
+  All: () => true,
+  Active: (task) => !task.completed,
+  Completed: (task) => task.completed,
+};
+
+const FILTER_NAMES = Object.keys(FILTER_MAP);
+
+function usePrevious(value) {
+  const ref = useRef();
+  useEffect(() => {
+    ref.current = value;
+  });
+  return ref.current;
+}
+
 function App(props) {
   const [tasks, setTasks] = useState(props.tasks);
 
+  const [filter, setFilter] = useState("All");
   function toggleTaskCompleted(id) {
     const updatedTasks = tasks.map((task) => {
       if (id === task.id) {
@@ -21,14 +38,36 @@ function App(props) {
     const remainingTasks = tasks.filter((task) => id !== task.id);
     setTasks(remainingTasks);
   }
-  const taskList = tasks?.map((task) => (
-    <Todo
-      id={task.id}
-      name={task.name}
-      completed={task.completed}
-      key={task.id}
-      toggleTaskCompleted={toggleTaskCompleted}
-      deleteTask={deleteTask}
+
+  function editTask(id, newName) {
+    const editedTaskList = tasks.map((task) => {
+      if (id === task.id) {
+        return { ...task, name: newName };
+      }
+      return task;
+    });
+    setTasks(editedTaskList);
+  }
+
+  const taskList = tasks
+    .filter(FILTER_MAP[filter])
+    .map((task) => (
+      <Todo
+        id={task.id}
+        name={task.name}
+        completed={task.completed}
+        key={task.id}
+        toggleTaskCompleted={toggleTaskCompleted}
+        deleteTask={deleteTask}
+        editTask={editTask}
+      />
+    ));
+  const filterList = FILTER_NAMES.map((name) => (
+    <FilterButton
+      key={name}
+      name={name}
+      isPressed={name === filter}
+      setFilter={setFilter}
     />
   ));
   const tasksNoun = taskList.length !== 1 ? "tasks" : "task";
@@ -36,6 +75,7 @@ function App(props) {
 
   function addTask(name) {
     const newTask = { id: `todo-${nanoid()}`, name, completed: false };
+    // console.log(newTask);
     setTasks((prev) => {
       return [...prev, newTask];
     });
@@ -43,16 +83,24 @@ function App(props) {
   // function toggleTaskCompleted(id) {
   //   console.log(tasks[0]);
   // }
+  const listHeadingRef = useRef(null);
+
+  const prevTaskLength = usePrevious(tasks.length);
+
+  useEffect(() => {
+    if (tasks.length < prevTaskLength) {
+      listHeadingRef.current.focus();
+    }
+  }, [tasks.length, prevTaskLength]);
+
   return (
     <div className="todoapp stack-large">
       <h1>TodoMatic</h1>
       <Form addTask={addTask} />
-      <div className="filters btn-group stack-exception">
-        <FilterButton name="All" clicked="true" />
-        <FilterButton name="Working" />
-        <FilterButton name="completed" />
-      </div>
-      <h2 id="list-heading">{headingText}</h2>
+      <div className="filters btn-group stack-exception">{filterList}</div>
+      <h2 id="list-heading" tabIndex="-1" ref={listHeadingRef}>
+        {headingText}
+      </h2>
       <ul
         role="list"
         className="todo-list stack-large stack-exception"
--- src/components/FilterButton.jsx
+++ src/components/FilterButton.jsx
@@ -3,7 +3,8 @@ function FilterButton(props) {
     <button
       type="button"
       className="btn toggle-btn"
-      aria-pressed={props.clicked}
+      aria-pressed={props.isPressed}
+      onClick={() => props.setFilter(props.name)}
     >
       <span className="visually-hidden">Show </span>
       <span>{props.name}</span>
